3dc7afc20f32cd313030d1843f3177fe3848245c
Admin <sir.nikolai00@mail.ru>
2026-03-15 09:36:01 +0300

    Я СДЕЛАЛ!!!

---
+++ spammanager/urls.py
@@ -0,0 +1,28 @@
+from django.urls import path
+from . import views
+
+app_name = 'spammanager'
+
+urlpatterns = [
+    # Главная
+    path('', views.HomeView.as_view(), name='home'),
+
+    # Клиенты
+    path('clients/', views.ClientListView.as_view(), name='client_list'),
+    path('clients/create/', views.ClientCreateView.as_view(), name='client_create'),
+    path('clients/<int:pk>/update/', views.ClientUpdateView.as_view(), name='client_update'),
+    path('clients/<int:pk>/delete/', views.ClientDeleteView.as_view(), name='client_delete'),
+
+    # Сообщения
+    path('messages/', views.MessageListView.as_view(), name='message_list'),
+    path('messages/create/', views.MessageCreateView.as_view(), name='message_create'),
+    path('messages/<int:pk>/update/', views.MessageUpdateView.as_view(), name='message_update'),
+    path('messages/<int:pk>/delete/', views.MessageDeleteView.as_view(), name='message_delete'),
+
+    # Рассылки
+    path('mailings/', views.MailingListView.as_view(), name='mailing_list'),
+    path('mailings/create/', views.MailingCreateView.as_view(), name='mailing_create'),
+    path('mailings/<int:pk>/', views.MailingDetailView.as_view(), name='mailing_detail'),
+    path('mailings/<int:pk>/update/', views.MailingUpdateView.as_view(), name='mailing_update'),
+    path('mailings/<int:pk>/delete/', views.MailingDeleteView.as_view(), name='mailing_delete'),
+]
