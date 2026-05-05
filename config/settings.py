89aacc4e79e16f0d7bf99226a22249bcce808cad
Nik <sir.nikolai00@mail.ru>
2026-05-05 23:31:05 +0300

    поменял настройки докера

--- config/settings.py
+++ config/settings.py
@@ -118,8 +118,8 @@ CACHES = {
     }
 }
 
-CELERY_BROKER_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
-CELERY_RESULT_BACKEND = os.getenv("REDIS_URL", "redis://localhost:6379/0")
+CELERY_BROKER_URL = os.getenv("REDIS_URL", "redis://redis:6379/0")
+CELERY_RESULT_BACKEND = os.getenv("REDIS_URL", "redis://redis:6379/0")
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
